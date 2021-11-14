02ca9f1b778493663bafb49df4bba49eab6a1d8c
denniskuhn <dennis@kuhn.geek.nz>
2021-11-14 13:22:38 +1300

    fix: gitPackage add/commit/push root module too

--- src/scripts/gitPackage.ts
+++ src/scripts/gitPackage.ts
@@ -1,7 +1,8 @@
 import chalk from 'chalk';
 import path from 'path';
-import simpleGit, { SimpleGit } from 'simple-git';
+import simpleGit, { CommitResult, PushResult, SimpleGit } from 'simple-git';
 import { getVersion } from './SolutionFile';
+import { sync as findParentDir } from 'find-parent-dir';
 
 const cwd = process.cwd();
 
@@ -36,40 +37,57 @@ export const getPackagePath = (solutionPath: string) =>
         )
     );
 
+const submitRoot = async (rootModule: string, message: string) => {
+    const relativePackage = path.relative(rootModule, cwd);
+    const rootName = path.basename(rootModule);
+
+    const git = simpleGit(rootModule);
+    console.log(
+        chalk.green('Git: ') +
+        message + ' ' +
+        chalk.greenBright(rootName) + ' ' +
+        chalk.yellowBright(relativePackage)
+    );
+    const added = await git.add(relativePackage);
+    logAdded(added);
+    const committed = await git.commit(message, relativePackage);
+    logCommited(committed);
+    const pushed = await git.push();
+    logPushed(pushed);
+}
+
 export const gitPackage = async (solutionPath: string, message: string) => {
     const version = getVersion(path.resolve(cwd, solutionPath));
     const packageFile = getPackagePath(solutionPath);
     const git: SimpleGit = simpleGit();
+    const rootModule = findParentDir(cwd, '.gitmodules');
+    const relativeRoot = rootModule ? path.relative(cwd, rootModule) : rootModule;
 
     console.log(
         chalk.green('Git: ') +
-        chalk.magenta(packageFile) + ' ' +
-        chalk.yellow(version) + ' ' +
-        message
+        chalk.magenta(packageFile) + ' ' + chalk.yellow(version) + ' ' +
+        message +
+        (relativeRoot ?
+            chalk.green(' Root: ') +
+            chalk.greenBright(relativeRoot)
+            :
+            ''
+        )
     );
     const added = await git.add(packageFile);
 
-    if (added) {
-        console.log(chalk.green('Git add: ') + chalk.yellow(added));
-    }
-    const {summary, ...committed} = await git.commit(message, packageFile);
-    console.log(
-        chalk.green('Git commited: ') +
-        (committed.branch ? chalk.yellow(committed.branch) : chalk.red('no-branch')) +
-        ' @ ' + 
-        (committed.commit ? chalk.yellowBright(committed.commit) : chalk.red('no-commit')) +
-        ': ' +
-        ( 0 === summary.changes + summary.deletions + summary.insertions ?
-            chalk.red('no changes')
-            :
-            chalk.magentaBright(
-                JSON.stringify(summary)
-                    .replace(/['\"{}]/g, '')
-                    .replace(/,/g, ', ')
-            )
-        )
-    );
+    logAdded(added);
+    const committed = await git.commit(message, packageFile);
+    logCommited(committed);
     const pushed = await git.push();
+    logPushed(pushed);
+
+    if (rootModule) {
+        submitRoot(rootModule, message);
+    }
+}
+
+const logPushed = (pushed: PushResult) =>
     console.log(
         chalk.green('Git pushed:') +
         (undefined === pushed.update ?
@@ -79,4 +97,26 @@ export const gitPackage = async (solutionPath: string, message: string) => {
             ' hash: ' + chalk.magenta(pushed.update.hash.from) + ' -> ' + chalk.magentaBright(pushed.update.hash.to)
         )
     );
-}
+
+
+const logCommited = (committed: CommitResult) =>
+    console.log(
+        chalk.green('Git commited: ') +
+        (committed.branch ? chalk.yellow(committed.branch) : chalk.red('no-branch')) +
+        ' @ ' +
+        (committed.commit ? chalk.yellowBright(committed.commit) : chalk.red('no-commit')) +
+        ': ' +
+        (0 === committed.summary.changes + committed.summary.deletions + committed.summary.insertions ?
+            chalk.red('no changes')
+            :
+            chalk.magentaBright(
+                JSON.stringify(committed.summary)
+                    .replace(/['\"{}]/g, '')
+                    .replace(/,/g, ', ')
+            )
+        )
+    );
+
+const logAdded = (added: string) =>
+    added &&
+    console.log(chalk.green('Git add: ') + chalk.yellow(added));
